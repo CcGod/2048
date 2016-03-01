cadfc58466145ef340127d204b25293368bf0e57
CcGod <html6@foxmail.com>
2016-03-01 11:26:07 +0800

    Get Browser information.

--- support.js
+++ support.js
@@ -4,6 +4,9 @@ var screenWidth = window.screen.availWidth,
 	cellSideLength = 0.18*screenWidth,
 	cellSpace = 0.04*screenWidth;
 
+var NV = {};
+var UA = navigator.userAgent.toLowerCase();
+
 function getPosTop(top,left){
 	return cellSpace+(cellSpace+cellSideLength)*top;
 }
@@ -143,4 +146,45 @@ function isEmpty(arr){
 			}
 	}
 	return true;
+}
+
+//获取浏览器内核信息
+function getBrowserIfo(){
+	try
+	{
+		NV.name=document.all?'ie':
+		(UA.indexOf("firefox")>0)?'firefox':
+		(UA.indexOf("chrome")>0)?'chrome':
+		window.opera?'opera':
+		window.openDatabase?'safari':
+		'unkonw';
+	}catch(e){};
+	try
+	{
+		NV.version=(NV.name=='ie')?UA.match(/msie ([\d.]+)/)[1]:
+		(NV.name=='firefox')?UA.match(/firefox\/([\d.]+)/)[1]:
+		(NV.name=='chrome')?UA.match(/chrome\/([\d.]+)/)[1]:
+		(NV.name=='opera')?UA.match(/opera.([\d.]+)/)[1]:
+		(NV.name=='safari')?UA.match(/version\/([\d.]+)/)[1]:
+		'0';
+	}catch(e){};
+	try
+	{
+		NV.shell=(UA.indexOf('360ee')>-1)?'360极速浏览器':
+		(UA.indexOf('360se')>-1)?'360安全浏览器':
+		(UA.indexOf('se')>-1)?'搜狗浏览器':
+		(UA.indexOf('aoyou')>-1)?'遨游浏览器':
+		(UA.indexOf('theworld')>-1)?'世界之窗浏览器':
+		(UA.indexOf('worldchrome')>-1)?'世界之窗极速浏览器':
+		(UA.indexOf('greenbrowser')>-1)?'绿色浏览器':
+		(UA.indexOf('qqbrowser')>-1)?'QQ浏览器':
+		(UA.indexOf('baidu')>-1)?'百度浏览器':
+		'未知或无壳';
+	}catch(e){}
+
+	console.log(UA);
+	console.log(NV.name);
+	console.log(NV.version);
+	console.log(NV.shell);
+	$("#browserIfo").text(UA+"-----"+NV.name+"-----"+NV.version+"-----"+NV.shell);
 }
